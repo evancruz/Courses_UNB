0d86c5c4d3828af999b0d07740d1f07dd59b0ac0
Moses Cruz <mcruz@Mosess-MacBook-Pro.local>
2020-01-17 21:36:59 -0400

    done rework

---
+++ 3413/A2/main.c
@@ -0,0 +1,406 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+ /*LISTS*/
+struct JobList{
+   char *user[100];
+   char *process[100];
+   int arrival;
+   int duration;
+   int runningTime;
+   int priority;
+   struct JobList *next;
+};
+
+struct User{
+   char *user[100];
+   int finishTime;
+   struct User *next;
+};
+
+//FunctionDeclarations
+void getJobRequests();
+void printJobList(struct JobList *head);
+struct JobList * nextJobRequest(int time);
+struct JobList * removeJob(struct JobList *node, struct JobList *head);
+void addToRunList(struct JobList *node);
+void insertRunList(struct JobList *job, int time);
+void insertFront(struct JobList *job, struct JobList *head);
+void insertBehind(struct JobList *node, struct JobList *node2);
+void nextTask();
+bool isDone(struct JobList *node);
+struct JobList * moveToBack(struct JobList *node, struct JobList *head);
+void createUser(char * user);
+void recordFinish(char * userName, int time);
+bool isSameNode(struct JobList *node1, struct JobList *node2);
+
+//constants and globals
+struct JobList *headJL = NULL;
+struct JobList *headRL = NULL;
+struct User *headUser = NULL;
+
+/***************************************************************
+ * MAIN                                                                                        *
+ *                                                                                                 *
+ * *************************************************************/
+//THINGS TO REMEMBER:
+//1. delete printf statements!!!!
+//2. fix insert front to be more code friendly (no copy pasta)
+//3. need to make an input where there is an idle spot before getting more jobs
+int main (int argc, char const *argv[]) {
+   if(argc != 2){
+      printf("Please provide 1 argument for the amount of CPU's \nExample: ./main 3\n");
+      exit(EXIT_FAILURE);
+   }
+   int const cores = ((int)*argv[1])-48;
+   int numCPU;
+
+   //fill JobList. HeadJL points to it
+   getJobRequests();
+
+   //Print Titles
+   printf("Time\t");
+   for(numCPU = 1; numCPU <= cores; numCPU++){
+      printf("CPU%d\t", numCPU);
+   }
+   printf("\n");
+
+   //start cpu time
+   int time = 0;
+   //run until both both Run List and Job List are empty
+   while(headRL || headJL){
+         struct JobList * job;
+
+         /*if there are still jobs left in JL then check for a job request
+         * Insert job requests into the Run List (RL)*/
+        NEXTREQUEST:
+         if(headJL){
+            while(job = nextJobRequest(time)){
+               //insert to RunList based on criteria. Remove from JL
+               insertRunList(job, time);
+               headJL = removeJob(job, headJL);
+               if(!headJL)
+                  break;
+            }
+         }         
+
+      if(headRL){
+         struct JobList *runPtr = headRL;
+         while(runPtr){
+            if(isDone(runPtr)){
+               struct JobList * node = runPtr;
+               runPtr = runPtr->next;
+               recordFinish(node->user, time);
+               headRL = removeJob(node, headRL);
+               //if nothing to run at the moment then break and see if there are more job requests
+               if(!headRL){
+                  if(!headJL)
+                     goto END;
+                  goto NEXTREQUEST;
+               }
+            } else {
+               runPtr = runPtr->next;
+            }
+         }
+
+        struct JobList * node = headRL;
+
+         //run through a cycle
+         node = headRL;
+         // printJobList(headRL);
+         printf("%d\t", time);
+         for(numCPU = 1; numCPU <= cores; numCPU++){
+            if(node){
+               printf("%s\t", node->process);
+               node->duration--;
+               node = node->next;
+            } else {
+               printf("-\t");
+            }
+         }
+         printf("\n");
+        
+      }//go to next cpu cycle
+
+      //after time++ continue? so i can put printidle?
+      time++;
+   }//end run time
+   END:
+   printf("%d\tIDLE\n\n", time);
+   printf("Summary\n");
+   struct User * node = headUser;
+   while(node){
+      printf("%s\t%d\n", node->user, node->finishTime);
+      node = node->next;
+   }
+
+   //free Users
+   node = headUser;
+   while(node){
+      node = node->next;
+      free(headUser);
+      headUser = node;
+   }
+}//end main
+
+
+
+
+/***************************************************************
+ * Functions                                                                                  *
+ *                                                                                                 *
+ * *************************************************************/
+
+void getJobRequests(){
+   int max = 1000;
+     //skips over the first line
+   char buffer[max];
+   //what happens if file is empty?
+   fgets(buffer, max, stdin);
+
+   while(fgets(buffer, max, stdin)){
+      if(headJL == NULL){
+         struct JobList *newJob = malloc(sizeof(struct JobList));
+         sscanf(buffer, "%s %s %d %d %d", newJob->user, newJob->process, &(newJob->arrival), &(newJob->duration), &(newJob->priority));
+         newJob->next = NULL;
+         headJL = newJob;
+      } else{
+         //there is at least one job. add new job to back
+         struct JobList *node = headJL;
+         while(node->next){
+            node = node->next;
+         }
+         //allocate memory for job
+         struct JobList *newJob = malloc(sizeof(struct JobList));
+         sscanf(buffer, "%s %s %d %d %d", newJob->user, newJob->process, &(newJob->arrival), &(newJob->duration), &(newJob->priority));
+         newJob->next = NULL;
+         node->next = newJob;
+      }
+   }
+}
+
+void printJobList(struct JobList *head){
+   if(head == NULL){
+      printf("NULL\n");
+   } else {
+      while(head){
+         printf("%s %s %d %d\n", head->user, head->process, head->arrival, head->duration, head->priority);
+         head = head->next;
+      }
+   }
+   printf("done print\n");
+}
+
+//returns a node from the JL list if time is equal to arrival
+struct JobList * nextJobRequest(int time) {
+   struct JobList *nodeJL = headJL;
+   while(nodeJL){
+      if(nodeJL->arrival == time){
+         return nodeJL;
+      }
+      nodeJL = nodeJL->next;
+   }
+   return NULL;
+}
+
+struct JobList * removeJob(struct JobList *node, struct JobList *head){
+   if(strcmp(node->process,head->process) == 0){
+      head = node->next;
+      free(node);
+      return head;
+   } else {
+      struct JobList *prev = head;
+      struct JobList *current = head->next;
+      while(current){
+         if(strcmp(node->process, current->process) == 0){
+            prev->next = current->next;
+            free(current);
+            return head;
+         }
+         prev = current;
+         current = current->next;
+      }
+      //if it reaches here then it means no job was removed
+      printf("no job was removed\n");
+      return head;
+   }
+}
+
+void insertRunList(struct JobList *job, int time){
+   //this runs after every job is added to runlist
+   if(!headRL){
+      insertFront(job, headRL);
+   }else{
+      struct JobList *node = headRL;
+      while(node->next){
+         if(node->arrival == job->arrival && node->duration > job->duration){
+            insertBehind(node, job);
+            return;
+         } else {
+            node = node->next;
+         }
+      } //if no node next we are at end
+      //make comparison. 
+      if(node->arrival == job->arrival && node->duration > job->duration){
+            insertBehind(node, job);
+         } else {
+            insertFront(job, headRL);
+         }
+   }
+}
+
+
+
+void insertFront(struct JobList *job, struct JobList *head){
+   struct JobList *runListTask = malloc(sizeof(struct JobList));
+   strcpy(runListTask->user, job->user);
+   strcpy(runListTask->process, job->process);
+   runListTask->arrival = job->arrival;
+   runListTask->duration = job->duration;
+   runListTask->runningTime = 0;
+   runListTask->next = NULL;
+   if(head == NULL){
+      headRL = runListTask;
+   } else {
+      struct JobList *endOfList = head;
+      while(endOfList->next){
+         endOfList= endOfList->next;
+      }
+      endOfList->next = runListTask;
+   }
+    createUser(runListTask->user);
+}
+
+/*
+*   node is RL. node2 is JL job
+*   case 1 behind head
+*   case 2 after head (middle node)
+*/
+void insertBehind(struct JobList *node, struct JobList *node2){
+
+   struct JobList *runListTask = malloc(sizeof(struct JobList));
+   strcpy(runListTask->user, node2->user);
+   strcpy(runListTask->process, node2->process);
+   runListTask->arrival = node2->arrival;
+   runListTask->duration = node2->duration;
+   runListTask->runningTime = 0;
+
+   if(strcmp(node->process, headRL->process)==0){
+      runListTask->next = node;
+      headRL = runListTask;
+   } else {
+      struct JobList *ptr = headRL;
+      while(ptr->next != node){
+         ptr=ptr->next;
+      }
+      ptr->next = runListTask;
+      runListTask->next = node;
+   }
+   createUser(runListTask->user);
+}
+
+//move task to the back and bring the next one in line to the front
+void nextTask(){
+   if(headRL->duration == 0){
+      struct JobList *nextJob = headRL->next;
+      free(headRL);
+      headRL = nextJob;
+   } else {
+      struct JobList *eofList = headRL->next;
+      //only 1 in run list
+      if(eofList == NULL){
+         return;
+      } else {
+         //get to end
+         while(eofList->next){
+            eofList = eofList->next;
+         }
+         //ensure headRL points to first node
+         struct JobList *nextJob = headRL->next;
+         eofList->next = headRL;
+         headRL->next = NULL;
+         headRL = nextJob;
+      }
+   }
+}
+
+
+bool isDone(struct JobList *node){
+   if(node->duration == 0) 
+      return true;
+   else
+      return false;
+}
+
+struct JobList * moveToBack(struct JobList *node, struct JobList *head){
+   //already at back
+   if(node->next == NULL)
+      return head;
+   //go through to back of list
+   struct JobList * eoList = head;
+   while(eoList->next){
+      eoList = eoList->next;
+   }
+   if(isSameNode(node, head)){
+      eoList->next = node;
+      head = node->next;
+      node->next = NULL;
+      return head;
+   } else {
+      //node is not at back nor is it the head
+     struct JobList * previousToNode = head;
+     while(previousToNode->next){
+        if(isSameNode(previousToNode->next, node)){
+           break;
+        }
+        previousToNode = previousToNode->next;
+     }
+     previousToNode->next = node->next;
+     eoList->next = node;
+     node->next = NULL;
+   }
+}
+
+bool isSameNode(struct JobList *node1, struct JobList *node2){
+   if(strcmp(node1->process, node2->process)==0)
+      return true;
+   else 
+      return false;
+}
+
+void recordFinish(char * userName, int time){
+   struct User *node= headUser;
+   while(node){
+      if(strcmp(node->user, userName) == 0){
+         node->finishTime = time;
+         return;
+      }
+      node = node->next;
+   }
+}
+
+void createUser(char * name){
+   if(!headUser){
+      headUser = malloc(sizeof(struct User));
+      strcpy(headUser->user, name);
+      headUser->next = NULL;
+   } else {
+      struct User *node = headUser;
+      while(node->next){
+         if(strcmp(node->user, name) ==0){
+            return;
+         }
+         node = node->next;
+      }
+      if(strcmp(node->user, name)==0){
+            return;
+      } else {
+         struct User *newUser = malloc(sizeof(struct User));
+         node->next = newUser;
+         strcpy(newUser->user, name);
+         newUser->next = NULL;
+      }
+   }
+}
